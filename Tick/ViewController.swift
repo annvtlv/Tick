2e5f8e0ad191f7e04979687b0abe19965668e307
Annvtlv <anna.erofeevskaya@gmail.com>
2025-08-25 11:57:12 +0300

    Initial Commit

---
+++ Tick/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tick
+//
+//  Created by Анна Ветелева on 25.08.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
